4c95438d6bce9a2d60ae7b5c06dd491fe0e4540b
gabrielangela <gabrielangelam29@gmail.com>
2025-07-16 19:08:05 +0700

    feat: restrict acces to login/register when user is logged in/is authenticated

--- start/src/layout/AdminLayout.jsx
+++ start/src/layout/AdminLayout.jsx
@@ -1,12 +1,25 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../configs/firebase';
 
 export default function AdminLayout() {
-    
-    return (
-        <>
-            <header>Admin Side</header>
-            <Outlet />
-        </>
-    );
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('/'); // redirect ke home kalau SUDAH login
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate]);
+
+  return (
+    <>
+      <header>Admin Side</header>
+      <Outlet />
+    </>
+  );
 }
